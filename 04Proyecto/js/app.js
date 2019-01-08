50f4e2d9b918a63030bb08cee650a59d478a9ca9
mathiasuy <mathias.battistella@fing.edu.uy>
2019-01-08 18:24:17 -0300

    actualizacion getNewId()

--- 04Proyecto/js/app.js
+++ 04Proyecto/js/app.js
@@ -31,18 +31,39 @@ app.factory('Expenses', function(){
 	var service = {};
 
 	service.entries = [
-		{description: 'food', amount: 10.00015, date: '2014-10-01'},
-		{description: 'tiCKetS', amount: 11, date: '2014-10-02'},
-		{description: 'a nice chair', amount: 12.11111, date: '2014-10-03'},
-		{description: 'Phone Credit', amount: 13, date: '2014-10-04'},
-		{description: 'bills', amount: 14, date: '2014-10-04'},
-		{description: 'Tomato', amount: 15, date: '2014-10-06'},
+		{id : 1, description: 'food', amount: 10.00015, date: '2014-10-01'},
+		{id : 2, description: 'tiCKetS', amount: 11, date: '2014-10-02'},
+		{id : 3, description: 'a nice chair', amount: 12.11111, date: '2014-10-03'},
+		{id : 4, description: 'Phone Credit', amount: 13, date: '2014-10-04'},
+		{id : 5, description: 'bills', amount: 14, date: '2014-10-04'},
+		{id : 6, description: 'Tomato', amount: 15, date: '2014-10-06'},
 	];
 
+	//convertir a objeto Date
+	service.entries.forEach(function(element){
+		element.date = new Date(element.date); 
+	});
+
+	//Para guardar
 	service.save = function(entry){
 		service.entries.push(entry);
 	}
 
+	//Para obtener un nuevo id:
+	service.getNewId = function(){
+		if (service.newId){
+			service.newId++;
+			return service.newId;
+		}else{
+			//funcion de la libreria Underscore que devuelve el elemento con entry.id más alto:
+			var entryMaxId = _.max(service.entries, function(entry){
+				return entry.id;
+			});
+			service.newId = entryMaxId.id + 1;
+			return service.newId;
+		}
+	}
+	
 	return  service;
 });
 
@@ -60,7 +81,8 @@ app.controller('ExpensesViewController',  ['$scope', 'Expenses', function($scope
 app.controller('ExpenseViewController', ['$scope', '$routeParams', '$location', 'Expenses', function($scope, $routeParams, $location, Expenses){
 	//$scope.someText = "The world is round "  + $routeParams.id + " The first entrie is " + Expenses.entries[0].description;
 	if (!$routeParams.id){
-		$scope.expense = {id : 7, description: 'algo', amount: 10, date: new Date()};
+		var newId = Expenses.getNewId();
+		$scope.expense = {id : newId, description: 'algo ' + newId, amount: 10, date: new Date()};
 	}
 
 	$scope.save = function() {
